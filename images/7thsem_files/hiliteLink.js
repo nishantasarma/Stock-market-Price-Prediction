e61e8470abf79f582fb4b0ccc4c1aee2542e3994
nishantasarma <nishantasarma@gmail.com>
2021-03-08 09:37:56 +0530

    Minor project done back in college

---
+++ images/7thsem_files/hiliteLink.js
@@ -0,0 +1,56 @@
+
+<link href="/sbijava/css/style.css" rel="stylesheet" type="text/css">
+  <table width="100%" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="564" height="72" rowspan="2" align="left" valign="top">&nbsp;<a href="http://www.statebankofindia.com" target="_blank"><img src="/sbijava/images/header_2_02.jpg" alt="State Bank of India Logo" width="242" height="72" border="0"></a></td>
+    <td width="12" valign="middle" id="top_1"><img src="/sbijava/images/right_link_curve.gif" width="12" height="24"></td>
+    <td width="330" height="24" valign="middle" nowrap class="topNavText" id="top_1">&nbsp;</td>
+    <td width="87" rowspan="2" align="right" valign="top"><img src="/sbijava/images/header_2_06.jpg" alt="OnlineSBI Logo"></td>
+  </tr>
+  <tr> 
+    <td height="51" colspan="2" align="center">&nbsp;</td>
+  </tr>
+</table>
+<!--Logo, Welcome Name Ends Here-->
+ <!--Top Nav with general links, Login,Logout starts Here-->
+      <table width="100%"  border="0" cellpadding="0" cellspacing="0" class="headerTopNav202">
+        <tr>
+			<td class="topNavTextNormal" height="22">&nbsp;</td>
+        </tr>
+      </table>
+       <table width="100%" border="0" cellpadding="0" cellspacing="0">
+          <tr>
+			<td width="50%" cope="col" class="maincurvecls"><h1 class="pageTitleHeaderlog"><a name="main"></a>Error</h1></td>
+            <td width="50%" class="maincurvecls" scope="col">
+              <table border="0" align="right" cellpadding="0" cellspacing="0" id="tblPageContent">
+                <tr>
+                  <td height="31" align="right" valign="middle" nowrap class="innerPageTopLeftBGWithText"></td>
+	 	 				<td width="14" height="31" align="right" valign="middle"></td>
+						<td width="29" height="31" align="right" valign="middle" class="helpAndPrintLinks"></td>
+						<td width="14" height="31" align="right" valign="middle"></td>
+						<td width="10" height="31" align="right" valign="middle" class="helpAndPrintLinks"></td>
+                </tr>
+				</table>
+			</td>
+          </tr>
+          <tr>
+            <td colspan="2" align="left" valign="top" scope="row" class="pageContentBG"> 
+            <table width="90%"  border="0" align="center" cellpadding="0" cellspacing="0">
+				<tr>
+					<td class="errorTxt">
+						<p>Due to technical problems we need to close this session. <br>We regret the inconvenience.</p><p> Please click here to login to <a href="icollecthome.htm">OnlineSBI</a></p></td>
+				</tr>
+				</table><br>
+			</td>
+          </tr>
+  </table>
+         <td colspan="2"></td>
+            <td colspan="6" align="left" valign="top" class="footerText">
+          <table width="100%" border="0" cellpadding="0" cellspacing="0" id="tblContainer">
+			<tr>
+				<td height="25" valign="middle" class="footerText">&copy;&nbsp;Copyright SBI.</td>
+			</tr>
+		</table>
+         </td>
+            
+        
